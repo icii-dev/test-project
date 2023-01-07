d84a6bafbc8f00b8bda5c49a78b63f6bf7847725
icii.dev <36289576+icii-dev@users.noreply.github.com>
2023-01-07 14:29:35 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
-function App() {
+const  App = () =>  {
+
+  const handleBtnWallet = () => {
+    // action buttons
+    console.log('button wallet clicked')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="btn" onClick={()=>handleBtnWallet()}>Wallet</div>
+        <div className="btn">Mint</div>
+
+        <div className="btn">Button 1</div>
+        <div className="btn">Button 2</div>
     </div>
   );
 }
